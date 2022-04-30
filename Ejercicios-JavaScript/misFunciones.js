8aa7cb32efc6bbca2c5ea37cbdc8319866463874
VickyVaccarini <vicky_vaccarini@hotmail.com>
2022-04-30 00:11:25 -0300

    Prueba Java

--- Ejercicios-JavaScript/misFunciones.js
+++ Ejercicios-JavaScript/misFunciones.js
@@ -5,7 +5,47 @@
  * @param {number} valor - El valor de los inputs de metros, pulgadas, pies o yardas.
  * @return
  */
+function cambiarUnidades (id, valor) {
+    var metro, pulgada, pie, yarda;
 
+    if(valor.includes(",")){ /*include es para ver si el valor ingresado incluye , */
+        valor=valor.replace(",", "."); /*aca se reemplaza la , por el .*/
+    }
+
+    if (isNaN(valor)){
+        alert("Se ingreso un valor invalido "+id);
+        metro = "";
+        pulgada = "";
+        pie = "";
+        yarda = "";
+    } else if (id=="metro"){
+        metro = valor;
+        pulgada = 39.3701*valor;
+        pie = 3.28084*valor;
+        yarda = 1.09361*valor;
+    } else if (id == "pulgada"){
+        pulgada = valor;
+        metro = 0.0254*valor;
+        pie = 0.0833333*valor;
+        yarda = 0.0277778*valor;
+    } else if (id=="yarda"){
+        yarda = valor;
+        metro = 0.9144*valor;
+        pulgada = 36*valor;
+        pie = 3*valor;
+    } else if (id=="pie"){
+        pie = valor;
+        metro = 0.3048*valor;
+        pulgada = 12*valor;
+        yarda = 0.333333*valor;
+    }
+    document.lasUnidades.unid_metro.value=Math.round(metro*100)/100; /*Math.round() es para redondear*/
+    document.lasUnidades.unid_pulgada.value=Math.round(pulgada*100)/100; /*  *100)/100 es para que tome 2 decimales*/
+    document.lasUnidades.unid_pie.value=Math.round(pie*100)/100;
+    document.lasUnidades.unid_yarda.value=Math.round(yarda*100)/100;
+}
+
+/*IGUAL QUE EL DE ARRIBA PERO ESTA REFACTORIZADO PARA QUE SEA MAS LEGITIMO
 function cambiarUnidades(id, valor){
     if(isNaN(valor)){
         alert("Se ingresó un valor inválido");
@@ -30,7 +70,7 @@ function cambiarUnidades(id, valor){
         document.lasUnidades.unid_pulgada.value=36*valor;
         document.lasUnidades.unid_pie.value=3*valor;
     }
-}
+}*/
 
 function convertirGR(id){
     var grad, rad;
